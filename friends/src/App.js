448148fbb4af48a7e66c84d255b7b1bcb7b9c433
Ihab Ali <ihab@live.com>
2019-06-12 06:54:02 -0500

    added friendslist component to render

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import FriendsList from "./components/FriendsList";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>React Friends</h1>
+          <FriendsList friends={this.state.friends} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
